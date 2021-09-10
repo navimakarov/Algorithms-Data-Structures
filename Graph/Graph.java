0c0459215b29d30a8acc788db5b1d8d27a48b2e5
navimakarov <makarov18042003@gmail.com>
2021-09-10 22:08:18 +0200

    Almost finished with graphs

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -350,7 +350,21 @@ public class Graph {
      * Shortest path between all vertexes
      */
     public void floyd() {
+        int dist[][] = new int[size][size];
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                dist[i][j] = matrix[i][j];
+            }
+        }
 
+        for(int k = 0; k < size; k++) {
+            for(int i = 0; i < size; i++) {
+                for(int j = 0; j < size; j++) {
+                    if(dist[i][k] + dist [k][j] < dist[i][j])
+                        dist[i][j] = dist[i][k] + dist [k][j];
+                }
+            }
+        }
     }
 
     // -------------------------------------------------------------------------
